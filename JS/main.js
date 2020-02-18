22e52f995cbe6769dbe42b12e4d664b1f7dddcba
Liam Best <liambbest@googlemail.com>
2020-02-18 16:17:03 +0100

    Ready for Javascript

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,14 @@ function ajaxCallData(url) {
 }
 
 ajaxCallData("https://api.myjson.com/bins/zyv02");
+
+// BOOK INSERT FUNCTION:
+// Loop over object/array using for-in for-of for-each loop (find most suitable)
+// Create container div element "flip-card"
+// Create flip-card-inner
+// Inside flip-card-inner create flip-card-front
+// Pull in book image from ajax call to take up flip-card-front
+// Inside flip-card-inner create flip-card-back
+// Pull in book title and description from ajax call
+// Create "more info" button
+// Repeat with loop for all books
